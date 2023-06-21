7e58edddde5204ec36b032e77ee549884a7489a0
Kumilachew-g <kumilachew1212@gmail.com>
2023-06-21 09:25:41 +0300

    Implement page route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 function App() {
   return (
     <div className='App'>
-      <h1>Hello React!</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
