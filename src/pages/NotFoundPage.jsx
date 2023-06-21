19b9ea94dc2742397da5764a037edf54f629c691
Kumilachew-g <kumilachew1212@gmail.com>
2023-06-21 17:00:13 +0300

    Update NotFoundPage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 export default function NotFoundPage() {
   return (
     <div>
-      <h1>404</h1>
+      <h1>404 Error</h1>
+      <h1>Page Not Found</h1>
     </div>
   );
 }
