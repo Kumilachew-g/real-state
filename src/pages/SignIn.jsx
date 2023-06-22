f6dfe294051412018ba276f0bb94a04e07923267
Kumilachew-g <kumilachew1212@gmail.com>
2023-06-22 15:23:43 +0300

    Update sign in pages

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export default function SignIn() {
-  return <div>Sign In</div>;
+  return (
+    <section>
+      <h1 className='text-3xl text-center mt-6 font-bold'>Sign In</h1>
+      <img
+        src='https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80'
+        alt='key'
+      />
+    </section>
+  );
 }
