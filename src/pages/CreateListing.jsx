0fc87c8bad9a58e03c78ebd556ebb5dfa4b3e3c0
Kumilachew-g <kumilachew1212@gmail.com>
2023-07-15 11:20:00 +0300

    Implement add functionality, onChange and onSubmit

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -9,8 +9,12 @@ import {
 } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 import { v4 as uuidv4 } from 'uuid';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreateListing() {
+  const navigate = useNavigate();
   const auth = getAuth();
   const [geolocationEnabled, setGeolocationEnabled] = useState(true);
   const [loading, setLoading] = useState(false);
@@ -36,8 +40,8 @@ export default function CreateListing() {
     bedrooms,
     bathrooms,
     parking,
-    furnished,
     address,
+    furnished,
     description,
     offer,
     regularPrice,
@@ -54,16 +58,14 @@ export default function CreateListing() {
     if (e.target.value === 'false') {
       boolean = false;
     }
-
-    // For Files
+    // Files
     if (e.target.files) {
       setFormData((prevState) => ({
         ...prevState,
         images: e.target.files,
       }));
     }
-
-    //  For text/boolean/number
+    // Text/Boolean/Number
     if (!e.target.files) {
       setFormData((prevState) => ({
         ...prevState,
@@ -74,22 +76,44 @@ export default function CreateListing() {
   async function onSubmit(e) {
     e.preventDefault();
     setLoading(true);
-    if (discountedPrice >= regularPrice) {
+    if (+discountedPrice >= +regularPrice) {
       setLoading(false);
       toast.error('Discounted price needs to be less than regular price');
       return;
     }
     if (images.length > 6) {
       setLoading(false);
-      toast.error('Maximum 6 images are allowed');
+      toast.error('maximum 6 images are allowed');
       return;
+    }
+    let geolocation = {};
+    let location;
+    if (geolocationEnabled) {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+      );
+      const data = await response.json();
+      console.log(data);
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+
+      location = data.status === 'ZERO_RESULTS' && undefined;
+
+      if (location === undefined) {
+        setLoading(false);
+        toast.error('please enter a correct address');
+        return;
+      }
+    } else {
+      geolocation.lat = latitude;
+      geolocation.lng = longitude;
     }
 
     async function storeImage(image) {
       return new Promise((resolve, reject) => {
         const storage = getStorage();
-        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
-        const storageRef = ref(storage, fileName);
+        const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+        const storageRef = ref(storage, filename);
         const uploadTask = uploadBytesResumable(storageRef, image);
         uploadTask.on(
           'state_changed',
@@ -123,14 +147,28 @@ export default function CreateListing() {
       });
     }
 
-    const imageUrls = await Promise.all(
+    const imgUrls = await Promise.all(
       [...images].map((image) => storeImage(image))
-    ).catch((errors) => {
+    ).catch((error) => {
       setLoading(false);
       toast.error('Images not uploaded');
       return;
     });
-    console.log(imageUrls);
+
+    const formDataCopy = {
+      ...formData,
+      imgUrls,
+      geolocation,
+      timestamp: serverTimestamp(),
+    };
+    delete formDataCopy.images;
+    !formDataCopy.offer && delete formDataCopy.discountedPrice;
+    delete formDataCopy.latitude;
+    delete formDataCopy.longitude;
+    const docRef = await addDoc(collection(db, 'listings'), formDataCopy);
+    setLoading(false);
+    toast.success('Listing created');
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`);
   }
 
   if (loading) {
@@ -138,7 +176,7 @@ export default function CreateListing() {
   }
   return (
     <main className='max-w-md px-2 mx-auto'>
-      <h1 className='text-3xl text-center mt-6 font-bold'>Create Listing</h1>
+      <h1 className='text-3xl text-center mt-6 font-bold'>Create a Listing</h1>
       <form onSubmit={onSubmit}>
         <p className='text-lg mt-6 font-semibold'>Sell / Rent</p>
         <div className='flex'>
@@ -192,7 +230,7 @@ export default function CreateListing() {
               min='1'
               max='50'
               required
-              className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center'
+              className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center'
             />
           </div>
           <div>
@@ -205,7 +243,7 @@ export default function CreateListing() {
               min='1'
               max='50'
               required
-              className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center'
+              className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center'
             />
           </div>
         </div>
@@ -231,7 +269,7 @@ export default function CreateListing() {
               parking ? 'bg-white text-black' : 'bg-slate-600 text-white'
             }`}
           >
-            No
+            no
           </button>
         </div>
         <p className='text-lg mt-6 font-semibold'>Furnished</p>
@@ -245,7 +283,7 @@ export default function CreateListing() {
               !furnished ? 'bg-white text-black' : 'bg-slate-600 text-white'
             }`}
           >
-            Yes
+            yes
           </button>
           <button
             type='button'
@@ -256,7 +294,7 @@ export default function CreateListing() {
               furnished ? 'bg-white text-black' : 'bg-slate-600 text-white'
             }`}
           >
-            No
+            no
           </button>
         </div>
         <p className='text-lg mt-6 font-semibold'>Address</p>
@@ -267,14 +305,12 @@ export default function CreateListing() {
           onChange={onChange}
           placeholder='Address'
           required
-          className='w-full px-4 py-2 text-xl text-gray-700 bg-white border
-          border-gray-300 rounded transition duration-150 ease-in-out
-          focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6'
+          className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6'
         />
         {!geolocationEnabled && (
           <div className='flex space-x-6 justify-start mb-6'>
-            <div className='div'>
-              <p>Latitude</p>
+            <div className=''>
+              <p className='text-lg font-semibold'>Latitude</p>
               <input
                 type='number'
                 id='latitude'
@@ -283,12 +319,11 @@ export default function CreateListing() {
                 required
                 min='-90'
                 max='90'
-                className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center
-                '
+                className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center'
               />
             </div>
-            <div className='div'>
-              <p>Longitude</p>
+            <div className=''>
+              <p className='text-lg font-semibold'>Longitude</p>
               <input
                 type='number'
                 id='longitude'
@@ -297,8 +332,7 @@ export default function CreateListing() {
                 required
                 min='-180'
                 max='180'
-                className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center
-                '
+                className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center'
               />
             </div>
           </div>
@@ -309,11 +343,9 @@ export default function CreateListing() {
           id='description'
           value={description}
           onChange={onChange}
-          placeholder='Address'
+          placeholder='Description'
           required
-          className='w-full px-4 py-2 text-xl text-gray-700 bg-white border
-          border-gray-300 rounded transition duration-150 ease-in-out
-          focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6'
+          className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6'
         />
         <p className='text-lg font-semibold'>Offer</p>
         <div className='flex mb-6'>
@@ -326,7 +358,7 @@ export default function CreateListing() {
               !offer ? 'bg-white text-black' : 'bg-slate-600 text-white'
             }`}
           >
-            Yes
+            yes
           </button>
           <button
             type='button'
@@ -337,12 +369,12 @@ export default function CreateListing() {
               offer ? 'bg-white text-black' : 'bg-slate-600 text-white'
             }`}
           >
-            No
+            no
           </button>
         </div>
         <div className='flex items-center mb-6'>
           <div className=''>
-            <p className='text-lg font-semibold'>Regular Price</p>
+            <p className='text-lg font-semibold'>Regular price</p>
             <div className='flex w-full justify-center items-center space-x-6'>
               <input
                 type='number'
@@ -350,7 +382,7 @@ export default function CreateListing() {
                 value={regularPrice}
                 onChange={onChange}
                 min='50'
-                max='50000000'
+                max='400000000'
                 required
                 className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center'
               />
@@ -362,13 +394,10 @@ export default function CreateListing() {
             </div>
           </div>
         </div>
-
-        {/* When the offer is true */}
-
         {offer && (
           <div className='flex items-center mb-6'>
             <div className=''>
-              <p className='text-lg font-semibold'>Discounted Price</p>
+              <p className='text-lg font-semibold'>Discounted price</p>
               <div className='flex w-full justify-center items-center space-x-6'>
                 <input
                   type='number'
@@ -376,7 +405,7 @@ export default function CreateListing() {
                   value={discountedPrice}
                   onChange={onChange}
                   min='50'
-                  max='50000000'
+                  max='400000000'
                   required={offer}
                   className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center'
                 />
